d350989fdac18df14dd38c3fffdf621978b1bdbe
perappu <stokoriel@gmail.com>
2025-03-27 18:26:04 -0500

    chore: make it play nicer with github pages

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,10 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
