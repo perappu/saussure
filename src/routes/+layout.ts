633a4a6b2d7bab35512b9e1d4d369927ef61fdff
Kazimir <stokoriel@gmail.com>
2025-04-19 15:48:45 -0500

    fix: potential fix for localization on live

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,16 +1,5 @@
-import { setLocale } from '$lib/paraglide/runtime';
-import { get } from 'svelte/store';
 import type { LayoutLoad } from './app/$types';
-import { locale, settings } from '$lib/stores';
 
 export const ssr = false;
 
-export const load: LayoutLoad = async () => {
-    if (get(settings).LANGUAGE) {
-        try {
-            setLocale(get(locale) as 'en' | 'es' | 'de');
-        } catch {
-            setLocale('en');
-        }
-    }
-};
+export const load: LayoutLoad = () => {};
--- src/routes/app/+layout.ts
+++ src/routes/app/+layout.ts
@@ -1,7 +1,16 @@
-import { token } from '$lib/stores';
+import { setLocale } from '$lib/paraglide/runtime';
+import { locale, settings, token } from '$lib/stores';
+import { get } from 'svelte/store';
 import type { LayoutLoad } from './$types';
 
 // put the token in a store
 export const load: LayoutLoad = async ({ data }) => {
     token.set(data.token);
+    if (get(settings).LANGUAGE) {
+        try {
+            setLocale(get(locale) as 'en' | 'es' | 'de');
+        } catch {
+            setLocale('en');
+        }
+    }
 };
