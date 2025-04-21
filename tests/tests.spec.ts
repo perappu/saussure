7d0470636f79fdd83ec13b26154bd7ad54276699
Kazimir <stokoriel@gmail.com>
2025-04-21 11:24:39 -0500

    fix: broken login tests?

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Authentication', () => {
-  test('show sign in page', async ({ page }) => {
+  test('show sign in page', async ({ page, context }) => {
     await page.goto('/');
 
     // Expects page to have login button
     await expect(page.getByTestId('login-button')).toBeVisible();
   });
 
-  test('app redirects to sign in page when non-authenticated', async ({ page }) => {
+  test('app redirects to sign in page when non-authenticated', async ({ page, context }) => {
     await page.goto('/app');
 
     // Expect redirect
@@ -22,7 +22,7 @@ test.describe('Authentication', () => {
     await expect(page.getByTestId('login-button')).toBeVisible();
   });
 
-  test('login with github and be redirected to app page', async ({ page }) => {
+  test('login with github and be redirected to app page', async ({ page, context }) => {
     test.setTimeout(120_000);
 
     await page.goto("/");
@@ -30,26 +30,31 @@ test.describe('Authentication', () => {
 
     await page.waitForLoadState("networkidle");
 
-    await page.locator("xpath=//input[@id='login_field']").fill(process.env.GH_TEST_LOGIN!);
-    await page.locator("xpath=//input[@id='password']").fill(process.env.GH_TEST_PASSWORD!);
-    await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+    //handling if github is asking to relogin
+    if (await page.locator("xpath=//input[@id='login_field']").isVisible()) {
+      await page.locator("xpath=//input[@id='login_field']").fill(process.env.GH_TEST_LOGIN!);
+      await page.locator("xpath=//input[@id='password']").fill(process.env.GH_TEST_PASSWORD!);
+      await page.getByRole('button', { name: 'Sign in', exact: true }).click();
 
-    await expect(async () => {
-      expect(page).toHaveURL('/');
-    }).toPass();
+      await page.waitForLoadState("networkidle");
+    }
 
-    await page.waitForLoadState("networkidle");
+    //handling for if github is asking to re-authorize
+    if (await page.isVisible("text='Authorize saussure local'")) {
+      await page.waitForTimeout(5000);
+      await page.locator("xpath=//button[contains(@class, 'js-oauth-authorize-btn')]").click();
+      await page.waitForLoadState("networkidle");
+    }
 
     //assert we got the token cookie
-    const context = await page.context();
     var cookies = await context.cookies();
     var token = cookies.find(c => c.name == 'token') ?? { name: '' };
     await expect(token.name).toBe('token');
 
-    //i don't know why, but playwright won't follow the same redirect as a normal browser
-    await page.getByTestId("login-button").click();
-
     await page.waitForLoadState("networkidle");
+
+    //i don't know why, but playwright won't follow the same redirect as a normal browser in this circumstance
+    await page.getByTestId("login-button").click();
 
     await expect(page).toHaveURL('/app');
   });
