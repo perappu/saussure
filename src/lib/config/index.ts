0a06b6f6aef65649a88b6a38e955c0a0f2c946d1
Kazimir <stokoriel@gmail.com>
2025-04-26 17:11:29 -0500

    chore: update default 11ty config

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -68,14 +68,16 @@ export const initSiteConfig = async () => {
 export const setConfigEleventy = async () => {
     let changes = get(settings);
 
-    changes.LAYOUT_DIRECTORY = '_includes/layouts';
+    changes.LAYOUT_DIRECTORY = 'src/_layouts';
     changes.BASE_LAYOUT = 'base.njk';
     changes.CHARACTER_LAYOUT = 'character.njk';
     changes.IMAGE_LAYOUT = 'image.njk';
     changes.LITERATURE_LAYOUT = 'literature.njk';
-    changes.CHARACTER_DIRECTORY = 'content/characters';
-    changes.IMAGE_DIRECTORY = 'content/images';
-    changes.LITERATURE_DIRECTORY = 'content/literature';
+    changes.CHARACTER_DIRECTORY = 'src/content/characters';
+    changes.IMAGE_DIRECTORY = 'src/content/images';
+    changes.LITERATURE_DIRECTORY = 'src/content/literatures';
+
+    changes.MEDIA_PATH = 'src/assets/images';
 
     settings.set(changes);
 };
